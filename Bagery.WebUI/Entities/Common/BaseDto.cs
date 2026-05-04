2a2805922fea4ecd2864b24c34daafff71398fd5
Burak Han Ulusoy <burakhanulusoy18@gmail.com>
2026-05-04 22:31:24 +0300

    Add project files.

---
+++ Bagery.WebUI/Entities/Common/BaseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Bagery.WebUI.Entities.Common
+{
+    public abstract class BaseDto
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+
+
+
+    }
+}
