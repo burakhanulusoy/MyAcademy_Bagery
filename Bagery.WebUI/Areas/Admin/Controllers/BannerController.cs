4ce0f1c033d74d8c4440536198c5db85f9986bf4
Burak Han Ulusoy <burakhanulusoy18@gmail.com>
2026-05-12 19:31:31 +0300

    last today

--- Bagery.WebUI/Areas/Admin/Controllers/BannerController.cs
+++ Bagery.WebUI/Areas/Admin/Controllers/BannerController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Bagery.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize]
+    [Authorize(Roles ="Admin")]
     public class BannerController(IMediator _mediator) : Controller
     {
         public async Task<IActionResult> Index()
--- Bagery.WebUI/MediatorPattern/Commands/UserCommands/LoginUserCommand.cs
+++ Bagery.WebUI/MediatorPattern/Commands/UserCommands/LoginUserCommand.cs
@@ -2,7 +2,7 @@
 
 namespace Bagery.WebUI.MediatorPattern.Commands.UserCommands
 {
-    public class LoginUserCommand:IRequest<List<string>>
+    public class LoginUserCommand:IRequest<IList<string>>
     {
         public string Email { get; set; }
         public string Password { get; set; }
