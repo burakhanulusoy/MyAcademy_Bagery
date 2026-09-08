22c26fdde87d889c3f5d5b0e41d6382063ff759c
Burak Han Ulusoy <burakhanulusoy18@gmail.com>
2026-09-08 15:06:21 +0300

    The promotion has been updated and has ended.

---
+++ Bagery.WebUI/ViewComponents/WebUIComponents/Promotion/_GetPromotionsQueryResultViewComponents.cs
@@ -0,0 +1,17 @@
+﻿using Bagery.WebUI.MediatorPattern.Queries.PromotionQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bagery.WebUI.ViewComponents.WebUIComponents.Promotion
+{
+    public class _GetPromotionsQueryResultViewComponents(IMediator mediator):ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var results = await mediator.Send(new GetPromotionsQuery());
+            return View(results);
+        }
+
+
+    }
+}
