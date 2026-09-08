1e3cabe9cf2069319bd034b8f38b48116b877a78
Burak Han Ulusoy <burakhanulusoy18@gmail.com>
2026-09-08 15:23:50 +0300

    Categories were fetched along with the products, and the entity was completed.

---
+++ Bagery.WebUI/ViewComponents/WebUIComponents/Category/_GetCategoriesWithProductsQueryResultViewComponents.cs
@@ -0,0 +1,18 @@
+﻿using Bagery.WebUI.MediatorPattern.Queries.CategoryQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Bagery.WebUI.ViewComponents.WebUIComponents.Category
+{
+    public class _GetCategoriesWithProductsQueryResultViewComponents(IMediator _mediator):ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var result = await _mediator.Send(new GetCategoriesWithProductsQuery());
+            return View(result);
+        }
+
+
+    }
+}
